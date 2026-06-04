a58f1c0bf779a25eba8f238ae6f4732d3d4ad9b7
ackzell <ackzell@gmail.com>
2026-06-04 17:46:37 +0200

    fix (docs-panel): removed min-w for mobile devices to have better responsiveness.

--- components/PanelDocs.vue
+++ components/PanelDocs.vue
@@ -45,7 +45,7 @@ router.beforeEach(() => {
 
 <template>
   <div h-full relative of-scroll>
-    <article ref="docsEl" class="prose" p6 h-full min-w-lg>
+    <article ref="docsEl" class="prose" p6 h-full>
       <ContentRenderer v-if="page" :key="page.id" :value="page" class="m-auto max-w-3xl" />
       <div mt8 py2 grid="~ cols-[1fr_1fr] gap-4" class="m-auto max-w-3xl">
         <div>
